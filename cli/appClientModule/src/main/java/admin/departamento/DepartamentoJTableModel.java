26d25d427c8d4223153b9cb99d9d4983830a59da
Armando Villalba <arma20044@gmail.com>
2015-04-20 19:09:52 -0400

    Departamento. Genero.

---
+++ cli/appClientModule/src/main/java/admin/departamento/DepartamentoJTableModel.java
@@ -0,0 +1,38 @@
+package src.main.java.admin.departamento;
+
+import java.awt.geom.Area;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.postgresql.jdbc2.ArrayAssistantRegistry;
+
+public class DepartamentoJTableModel extends AbstractTableModel {
+
+	List<Object[]> ciudades = new ArrayList<Object[]>();
+	
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return ciudades.size();
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+
+		Object resultado = ciudades.get(rowIndex) [columnIndex];
+		
+		return resultado;
+	}
+
+	private String[] colNames = new String[] {"ID", "Nro. Departamento", "Desc. Departamento"};
+
+	@Override
+	public String getColumnName(int col) {
+	    return colNames[col];
+	}
+}
---
+++ cli/appClientModule/src/main/java/admin/genero/GeneroJTableModel.java
@@ -0,0 +1,38 @@
+package src.main.java.admin.genero;
+
+import java.awt.geom.Area;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.postgresql.jdbc2.ArrayAssistantRegistry;
+
+public class GeneroJTableModel extends AbstractTableModel {
+
+	List<Object[]> ciudades = new ArrayList<Object[]>();
+	
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return ciudades.size();
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+
+		Object resultado = ciudades.get(rowIndex) [columnIndex];
+		
+		return resultado;
+	}
+
+	private String[] colNames = new String[] {"ID", "Codigo", "Descripcion"};
+
+	@Override
+	public String getColumnName(int col) {
+	    return colNames[col];
+	}
+}
